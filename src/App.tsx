aa0db29d8448022decda6875d439a27fc6a20222
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-01-09 23:52:20 +0300

    local storage

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,42 @@ import {Counter} from "./components/Counter";
 
 function App() {
 
-    const maxCounterValue = 5
-    const minCounterValue = 0
-    const [counter, setCounter] = useState<number>(0);
+    const [value, setValue] = useState<number>(0)
 
-    const increaseCounter = () => {
-        if (counter < maxCounterValue) {
-            setCounter(counter + 1);
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterKey', JSON.stringify(value))
+        localStorage.setItem('counterKey + 1', JSON.stringify(value + 1))
+    }
+
+    const getToLocalStorageHandler = () => {
+
+        const valueAsString = localStorage.getItem('counterKey')
+        if (valueAsString) {
+            setValue(JSON.parse(valueAsString))
         }
     }
 
-    const resetCounter = () => {
-        setCounter(0);
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
     }
 
+    const removeItemFromLocalStorageHandler = () => {
+        localStorage.removeItem('counterKey + 1')
+    }
 
     return (
         <div className="App">
-            <Counter
-                counter={counter}
-                increaseCounter={increaseCounter}
-                resetCounter={resetCounter}
-                maxCounterValue={maxCounterValue}
-                minCounterValue={minCounterValue}
-            />
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getToLocalStorageHandler}>getToLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+            <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
         </div>
     );
 }
